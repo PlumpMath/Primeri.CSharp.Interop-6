139bb6eb3cd7f725721af45462dc67e99687bb3b
Лъчезар Петков <lu4i@abv.bg>
2015-09-13 15:42:58 +0300

    Модул 09 - Видео 03 - Добавяне на клас от данни

--- Interop/Interop/DataStruct.cs
+++ Interop/Interop/DataStruct.cs
@@ -1,12 +1,66 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Excel
 {
 	public class DataStruct
 	{
+		public List<DataRow> table = new List<DataRow> ();
 		public DataStruct ()
 		{
+			
+		}
+		public void addRow (string _fname, string _lname, string _age)
+		{
+			table.Add (new DataRow (_fname, _lname, _age));
+		}
+		public void printTable ()
+		{
+			try{
+				foreach (DataRow row in table)
+				{
+					Console.WriteLine(row.firstName + " " + row.lastName + ", " + row.age);
+				}
+
+
+			}catch{
+			}
+		}
+
+
+
+
+	}
+	public class DataRow
+	{
+		public string _firstName = "";
+		public string _lastName = "";
+		public string _age = "";
+
+
+		public DataRow (string __firstName, string __lastName, string __age)
+		{
+			_firstName = __firstName; _lastName = __lastName; _age = __age;
+		}
+
+		public string firstName
+		{
+			set {_firstName = value; }
+			get {return _firstName; }
+		}
+
+		public string lastName
+		{
+			set {_lastName = value; }
+			get {return _lastName; }
+		}
+
+		public string age
+		{
+			set {_age = value; }
+			get {return _age; }
 		}
 	}
+
 }
 
--- Interop/Interop/Program.cs
+++ Interop/Interop/Program.cs
@@ -8,8 +8,19 @@ namespace Excel
 		{
 			DataStruct data = new DataStruct ();
 			IOWrite write = new IOWrite (data);
-			Console.WriteLine ("hi");
+
+
+			//Набиране на данни в основната таблица
+			data.addRow("Мартин","Симеонов","33");
+			data.addRow("Георги","Ангелов","35");
+			data.addRow("Павлин","Михов","45");
+
+			//Проверка на таблицата
+			data.printTable();
+
 
 		}
+
+
 	}
 }
