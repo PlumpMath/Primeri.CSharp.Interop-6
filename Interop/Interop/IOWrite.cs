8fa5db87ea7ce3f97dadc63496bc77d5ed001b8e
Лъчезар Петков <lu4i@abv.bg>
2015-09-13 16:15:49 +0300

    Модул 09 - Видео 04 - Добавяне на клас за данни

--- Interop/Interop/IOWrite.cs
+++ Interop/Interop/IOWrite.cs
@@ -9,6 +9,39 @@ namespace Excel
 		{
 			
 		}
+		public bool exportTable()
+		{
+			try{
+				//междинни проверки
+
+
+				return true;
+				}catch
+			{
+				return false;
+			}
+		}
+
+		public void addRow(DataRow _row)
+		{
+			try{
+			
+
+			}catch{
+			}
+		}
+		public virtual void runFile()
+		{
+			try{
+				System.Diagnostics.Process.Start(getPath());
+
+			}catch{
+			}
+		}
+		private string getPath ()
+		{
+			return System.IO.Path.Combine (AppDomain.CurrentDomain.BaseDirectory, "table1.xlsx");
+		}
 	}
 }
 
